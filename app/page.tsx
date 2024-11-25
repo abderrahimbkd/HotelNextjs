3bd163a1d753405f8042469d815aed3c797915a5
abderrahimbkd <80970245+abderrahimbkd@users.noreply.github.com>
2024-11-25 14:40:16 +0100

    commit dinamic

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,14 @@ import Container from '@/app/components/Container';
 import ListingCard from '@/app/components/listings/ListingCard';
 import EmptyState from '@/app/components/EmptyState';
 
-import getListings, {
-  IListingsParams,
-} from '@/app/actions/getListings';
+import getListings, { IListingsParams } from '@/app/actions/getListings';
 import getCurrentUser from '@/app/actions/getCurrentUser';
 import ClientOnly from './components/ClientOnly';
 
 interface HomeProps {
   searchParams: IListingsParams;
 }
+export const dynamic = 'force-dynamic';
 
 const Home = async ({ searchParams }: HomeProps) => {
   const listings = await getListings(searchParams);
@@ -38,14 +37,9 @@ const Home = async ({ searchParams }: HomeProps) => {
             xl:grid-cols-5
             2xl:grid-cols-6
             gap-8
-          "
-        >
+          ">
           {listings.map((listing: any) => (
-            <ListingCard
-              currentUser={currentUser}
-              key={listing.id}
-              data={listing}
-            />
+            <ListingCard currentUser={currentUser} key={listing.id} data={listing} />
           ))}
         </div>
       </Container>
